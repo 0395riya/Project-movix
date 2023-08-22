c4849ea3f035398d5b5b7ae45513e66f0fed24a8
Archana <99121118+0395riya@users.noreply.github.com>
2023-08-22 16:28:05 +0530

    Update Videos.jsx

--- src/pages/details/videos/Videos.jsx
+++ src/pages/details/videos/Videos.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import './style.scss'
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import VideoPopup from '../../../components/videoPopup/VideoPopup'
-import Img from '../../../components/lazyLoadImage/img'
+import Img from '../../../components/lazyLoadImage/Img'
 import Playbtn from '../Playbtn'
 
 const VideosSection = ({data, loading}) => {
